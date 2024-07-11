d60793783b7ace3c9a4191fd0bad3cdcc567aaaf
iode13 <rousseleva1@gmail.com>
2024-07-11 04:56:47 +0200

    compiled

---
+++ webhooks.test.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+declare const fs: any;
+declare const webhook: any;
+declare const requestBody: string;
+declare const requestBodyBuffer: Buffer;
+declare const webhookSecret = "ED7D658C-D8EB-4941-948B-3973214F2D49";
+declare const signatureHeader = "2693754819d3e32d7e8fcb13c729631f316c6de8dc1cf634d6527f1c07276e7e";
